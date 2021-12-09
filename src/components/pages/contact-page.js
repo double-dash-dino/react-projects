2aef68a30cc923bd8ffbbfb7c74a03a608c49cec
Lionel De Swarte--Danko (He/him) <64731565+double-dash-dino@users.noreply.github.com>
2021-12-09 08:55:27 +0000

    add links

--- src/components/pages/contact-page.js
+++ src/components/pages/contact-page.js
@@ -5,15 +5,30 @@ const ContactPage = () => {
     <div className="contact-page">
       <h1>Contact me</h1>
       <div className="contact-links">
-        <button className="contact-button">
+        <a
+          className="contact-button"
+          target="_blank"
+          rel="noreferrer"
+          href="https://www.linkedin.com/in/lionel-de-swarte-danko-426b0b13a/"
+        >
           <i className="fa fa-linkedin fa-2x"></i>
-        </button>
-        <button className="contact-button">
+        </a>
+        <a
+          className="contact-button"
+          target="_blank"
+          rel="noreferrer"
+          href="https://github.com/double-dash-dino"
+        >
           <i className="fa fa-github fa-2x"></i>
-        </button>
-        <button className="contact-button">
+        </a>
+        <a
+          className="contact-button"
+          target="_blank"
+          rel="noreferrer"
+          href="mailto:lionel.dsw@gmail.com"
+        >
           <i className="fa fa-envelope fa-2x"></i>
-        </button>
+        </a>
       </div>
     </div>
   );
