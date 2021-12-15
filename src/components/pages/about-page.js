cfe939926affde0d496663d426d11b68293df09a
Lionel De Swarte--Danko (He/him) <64731565+double-dash-dino@users.noreply.github.com>
2021-12-15 16:13:52 +0000

    get unfurl logic to 4 sections

--- src/components/pages/about-page.js
+++ src/components/pages/about-page.js
@@ -4,16 +4,14 @@ import React, { useState } from "react";
 const AboutPage = () => {
   const [toUnfurl, setToUnfurl] = useState([]);
 
-  const workUnfurlHandler = () => {
-    if (toUnfurl.includes("workSection")) {
-      console.log("here");
-
+  const unfurlHandler = (smt) => {
+    if (toUnfurl.includes(smt.target.id)) {
       setToUnfurl((toUnfurl) => {
-        return toUnfurl.filter((item) => item !== "workSection");
+        return toUnfurl.filter((item) => item !== smt.target.id);
       });
     } else {
       setToUnfurl((toUnfurl) => {
-        return ["workSection", ...toUnfurl];
+        return [smt.target.id, ...toUnfurl];
       });
     }
   };
@@ -23,18 +21,61 @@ const AboutPage = () => {
       <h1>About me</h1>
       <div className="about-sections">
         <div className="section__work">
-          <div className="section__work__title" onClick={workUnfurlHandler}>
+          <div
+            className="section__work__title"
+            id="work-section"
+            onClick={unfurlHandler}
+          >
             Work
           </div>
-          {toUnfurl.includes("workSection") && (
+          {toUnfurl.includes("work-section") && (
             <div className="section__work__body">
               Lorem ipsum sic dolor amet
             </div>
           )}
         </div>
-        <div className="section__education">Education</div>
-        <div className="section__programming">Programming</div>
-        <div className="section__personal-interests">Personal interests</div>
+        <div className="section__education">
+          <div
+            className="section__education__title"
+            id="education-section"
+            onClick={unfurlHandler}
+          >
+            Education
+          </div>
+          {toUnfurl.includes("education-section") && (
+            <div className="section__education__body">
+              Lorem ipsum sic dolor amet
+            </div>
+          )}
+        </div>
+        <div className="section__programming">
+          <div
+            className="section__programming__title"
+            id="programming-section"
+            onClick={unfurlHandler}
+          >
+            Programming
+          </div>
+          {toUnfurl.includes("programming-section") && (
+            <div className="section__programming__body">
+              Lorem ipsum sic dolor amet
+            </div>
+          )}
+        </div>
+        <div className="section__personal-interests">
+          <div
+            className="section__personal-interests__title"
+            id="personal-interests-section"
+            onClick={unfurlHandler}
+          >
+            Personal interests
+          </div>
+          {toUnfurl.includes("personal-interests-section") && (
+            <div className="section__personal-interests__body">
+              Lorem ipsum sic dolor amet
+            </div>
+          )}
+        </div>
       </div>
     </div>
   );
