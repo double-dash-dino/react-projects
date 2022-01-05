fe1c40569640255c8af07b88577bb3a9c16134dd
Lionel De Swarte--Danko (He/him) <64731565+double-dash-dino@users.noreply.github.com>
2022-01-05 16:36:59 +0000

    add the structure

--- src/components/apps/study-timer.js
+++ src/components/apps/study-timer.js
@@ -1,10 +1,32 @@
 import { useState } from "react";
 import "./study-timer.css";
+import ClearButton from "../UI/ClearButton";
 
 const StudyTimer = (props) => {
+  const clearApp = () => {
+    props.onClearApp();
+  };
   return (
     <div className="study-timer">
-      <h1>Timer</h1>
+      <ClearButton onClearApp={clearApp} />
+      <div className="timer-box">
+        <div className="timer-title">
+          <h1>Study timer</h1>
+        </div>
+        <div className="timer-settings">
+          <div className="break-length-setting">Break length</div>
+          <div className="session-length-setting">Session length</div>
+        </div>
+        <div className="timer-countdown">
+          Session
+          <br />
+          25:00
+        </div>
+        <div className="timer-controls">
+          <button className="timer-control">Start / Pause</button>
+          <button className="timer-control">Reset</button>
+        </div>
+      </div>
     </div>
   );
 };
