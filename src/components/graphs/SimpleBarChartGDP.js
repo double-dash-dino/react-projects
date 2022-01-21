cd79ae8280ae23c7432f8b85a7e61cb6b64fa282
Lionel De Swarte--Danko (He/him) <64731565+double-dash-dino@users.noreply.github.com>
2022-01-21 18:59:32 +0000

    trying to firgure out how to integrate D3 in a react component

--- src/components/graphs/SimpleBarChartGDP.js
+++ src/components/graphs/SimpleBarChartGDP.js
@@ -6,19 +6,29 @@ import * as d3 from 'd3'
 const SimpleBarChartGDP = (props) => {
 
 const dataset = [3,5,6]
+const buildGraph = () => {
+d3.select('svg')
+.attr('background-color', 'white')
+// .append('h2')
+// .text('test')
+// .attr('x', 100)
+// .attr('y', 100)
+// .attr('color', 'white')
+// .attr('background-color', 'white')
+// .attr('font-size', '25px')
+}
     return (
+
        
         <div className="simple-bar-chart">
-             <script>
-            {d3.data(dataset) }
-            </script>
             bar chart here
             <svg
             style={{
-                height: 500,
-                width: "100%",
+                height: 250,
+                width: 250,
                 marginLeft: "0px",
                 marginRight:"0px",
+                backgroundColor:'blue'
             }}
             >
 <g className="plot-area" />
@@ -26,7 +36,11 @@ const dataset = [3,5,6]
 <g className="y-axis" />
 
 
+
             </svg>
+
+            <button onClick={buildGraph}>Build graph</button>
+             
         </div>
     )
 }
