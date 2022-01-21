e3bde443466b704e578f4fdab89ad4ac8f1265d9
Lionel De Swarte--Danko (He/him) <64731565+double-dash-dino@users.noreply.github.com>
2022-01-21 18:38:36 +0000

    add d3

--- src/components/graphs/SimpleBarChartGDP.js
+++ src/components/graphs/SimpleBarChartGDP.js
@@ -1,9 +1,17 @@
+import D3Graphs from '../apps/D3Graphs';
 import './SimpleBarChartGDP.css'
+import * as d3 from 'd3'
 
 
 const SimpleBarChartGDP = (props) => {
+
+const dataset = [3,5,6]
     return (
+       
         <div className="simple-bar-chart">
+             <script>
+            {d3.data(dataset) }
+            </script>
             bar chart here
         </div>
     )
