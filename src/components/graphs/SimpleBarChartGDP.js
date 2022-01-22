e4d3a790070b5068eaf8d5a01731ceeb3c9df33d
Lionel De Swarte--Danko <64731565+double-dash-dino@users.noreply.github.com>
2022-01-22 16:12:52 +0000

    get the chart to display

--- src/components/graphs/SimpleBarChartGDP.js
+++ src/components/graphs/SimpleBarChartGDP.js
@@ -1,59 +1,61 @@
 import "./SimpleBarChartGDP.css";
 import * as d3 from "d3";
-import React, { useEffect } from "react";
+import React, { useState, useEffect } from "react";
 
 const SimpleBarChartGDP = (props) => {
-  let dataset = [];
+  console.log("rendering the dom");
+  const [datasetUS, setDatasetUS] = useState("");
 
-  fetch(
-    "https://raw.githubusercontent.com/freeCodeCamp/ProjectReferenceData/master/GDP-data.json"
-  )
-    .then((response) => response.json())
-    .then((data) => {
-      dataset = data;
-    });
-
-  const dataPoints = dataset.data;
+  if (datasetUS === "") {
+    fetch(
+      "https://raw.githubusercontent.com/freeCodeCamp/ProjectReferenceData/master/GDP-data.json"
+    )
+      .then((response) => response.json())
+      .then((data) => {
+        setDatasetUS(data);
+      });
+  }
 
   useEffect(() => {
-    let barHeight = 1;
-    d3.select("svg")
-      .selectAll("rect")
-      .data(dataset.data)
-      .enter()
-      .append("rect")
-      .attr("width", "1px")
-      .attr("height", barHeight + "px")
-      .attr("background-color", "black");
-    {
-      barHeight++;
-    }
-    //   .attr("x", 0 + i)
-    //   .attr("y", 0)
+    let datasetTest = [
+      ["one", 1],
+      ["two", 2],
+      ["three", 3],
+    ];
 
-    //   .attr("background-color", "white")
-    //   .append("h2")
-    //   .attr("font-size", "25px")
-    //   .text("test")
-    //   .attr("width", "25px")
-    //   .attr("height", "25px")
-    //   .attr("x", 100)
-    //   .attr("y", 100)
-    //   .attr("color", "white")
-    //   .attr("background-color", "white")
-    //   .attr("font-size", "25px");
-  });
+    if (datasetUS !== "") {
+      console.log("type:", typeof datasetUS, datasetUS.data);
+      d3.select("svg")
+        .selectAll("rect")
+        //   .data(datasetTest)
+        .data(datasetUS.data)
+        .enter()
+        .append("rect")
+        .attr("width", "2px")
+        .attr("height", (d) => d[1] / 10 + "px")
+        .attr("background-color", "black")
+        .attr("x", (d, i) => i * 3)
+        .attr("y", (d) => 500 - d[1] / 10);
+    }
+  }, [datasetUS]);
 
   const buildGraph = () => {
-    console.log(dataset.data[0][1]);
+    fetch(
+      "https://raw.githubusercontent.com/freeCodeCamp/ProjectReferenceData/master/GDP-data.json"
+    )
+      .then((response) => response.json())
+      .then((data) => {
+        setDatasetUS(data);
+      });
+    // console.log(typeof datasetUS.data[0][1], datasetUS.data, datasetUS);
   };
   return (
     <div className="simple-bar-chart">
       bar chart here
       <svg
         style={{
-          height: 250,
-          width: 250,
+          height: 500,
+          width: "100%",
           marginLeft: "0px",
           marginRight: "0px",
           backgroundColor: "blue",
