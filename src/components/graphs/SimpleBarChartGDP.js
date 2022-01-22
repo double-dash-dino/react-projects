198bf1376e82a5cbb5e0f5d18652d9c5de481e38
Lionel De Swarte--Danko <64731565+double-dash-dino@users.noreply.github.com>
2022-01-22 15:17:26 +0000

    add fetch request

--- src/components/graphs/SimpleBarChartGDP.js
+++ src/components/graphs/SimpleBarChartGDP.js
@@ -1,65 +1,71 @@
-
-import './SimpleBarChartGDP.css'
-import * as d3 from 'd3'
-import React, {useEffect} from 'react'
-
+import "./SimpleBarChartGDP.css";
+import * as d3 from "d3";
+import React, { useEffect } from "react";
 
 const SimpleBarChartGDP = (props) => {
+  let dataset = [];
 
-// const dataset = [3,5,6]
+  fetch(
+    "https://raw.githubusercontent.com/freeCodeCamp/ProjectReferenceData/master/GDP-data.json"
+  )
+    .then((response) => response.json())
+    .then((data) => {
+      dataset = data;
+    });
 
+  const dataPoints = dataset.data;
 
-useEffect(() => {
+  useEffect(() => {
+    let barHeight = 1;
+    d3.select("svg")
+      .selectAll("rect")
+      .data(dataset.data)
+      .enter()
+      .append("rect")
+      .attr("width", "1px")
+      .attr("height", barHeight + "px")
+      .attr("background-color", "black");
+    {
+      barHeight++;
+    }
+    //   .attr("x", 0 + i)
+    //   .attr("y", 0)
 
-d3.select('svg')
-.attr('background-color', 'white')
-.append('h2')
-.attr("font-size", "25px")
-.text('test')
-.attr('x', 100)
-.attr('y', 100)
-.attr('color', 'white')
-.attr('background-color', 'white')
-.attr('font-size', '25px')
-})
+    //   .attr("background-color", "white")
+    //   .append("h2")
+    //   .attr("font-size", "25px")
+    //   .text("test")
+    //   .attr("width", "25px")
+    //   .attr("height", "25px")
+    //   .attr("x", 100)
+    //   .attr("y", 100)
+    //   .attr("color", "white")
+    //   .attr("background-color", "white")
+    //   .attr("font-size", "25px");
+  });
 
-
-const buildGraph = () => {
-d3.select('svg').attr('background-color', 'white')
-// .append('h2')
-// .text('test')
-// .attr('x', 100)
-// .attr('y', 100)
-// .attr('color', 'white')
-// .attr('background-color', 'white')
-// .attr('font-size', '25px')
-}
-    return (
-
-       
-        <div className="simple-bar-chart">
-            bar chart here
-            <svg
-            style={{
-                height: 250,
-                width: 250,
-                marginLeft: "0px",
-                marginRight:"0px",
-                backgroundColor:'blue'
-            }}
-            >
-<g className="plot-area" />
-<g className="x-axis" />
-<g className="y-axis" />
-
-
-
-            </svg>
-
-            <button onClick={buildGraph}>Build graph</button>
-             
-        </div>
-    )
-}
+  const buildGraph = () => {
+    console.log(dataset.data[0][1]);
+  };
+  return (
+    <div className="simple-bar-chart">
+      bar chart here
+      <svg
+        style={{
+          height: 250,
+          width: 250,
+          marginLeft: "0px",
+          marginRight: "0px",
+          backgroundColor: "blue",
+        }}
+      >
+        <g className="plot-area" />
+        <g className="x-axis" />
+        <g className="y-axis" />
+      </svg>
+      <button onClick={buildGraph}>Testing button</button>
+    </div>
+  );
+};
 
 export default SimpleBarChartGDP;
