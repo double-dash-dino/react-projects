d36d7ba5431e9c689ffb8ad8247534e62c9f6f49
Lionel De Swarte--Danko <64731565+double-dash-dino@users.noreply.github.com>
2022-01-22 16:18:18 +0000

    refactor

--- src/components/graphs/SimpleBarChartGDP.js
+++ src/components/graphs/SimpleBarChartGDP.js
@@ -3,7 +3,6 @@ import * as d3 from "d3";
 import React, { useState, useEffect } from "react";
 
 const SimpleBarChartGDP = (props) => {
-  console.log("rendering the dom");
   const [datasetUS, setDatasetUS] = useState("");
 
   if (datasetUS === "") {
@@ -16,43 +15,30 @@ const SimpleBarChartGDP = (props) => {
       });
   }
 
-  useEffect(() => {
-    let datasetTest = [
-      ["one", 1],
-      ["two", 2],
-      ["three", 3],
-    ];
+  const generateGraph = (dataset) => {
+    d3.select("svg")
+      .selectAll("rect")
+      .data(dataset.data)
+      .enter()
+      .append("rect")
+      .attr("width", "2px")
+      .attr("height", (d) => d[1] / 10 + "px")
+      .attr("background-color", "black")
+      .attr("x", (d, i) => i * 3)
+      .attr("y", (d) => 500 - d[1] / 10);
+  };
 
+  useEffect(() => {
     if (datasetUS !== "") {
-      console.log("type:", typeof datasetUS, datasetUS.data);
-      d3.select("svg")
-        .selectAll("rect")
-        //   .data(datasetTest)
-        .data(datasetUS.data)
-        .enter()
-        .append("rect")
-        .attr("width", "2px")
-        .attr("height", (d) => d[1] / 10 + "px")
-        .attr("background-color", "black")
-        .attr("x", (d, i) => i * 3)
-        .attr("y", (d) => 500 - d[1] / 10);
+      generateGraph(datasetUS);
     }
   }, [datasetUS]);
 
-  const buildGraph = () => {
-    fetch(
-      "https://raw.githubusercontent.com/freeCodeCamp/ProjectReferenceData/master/GDP-data.json"
-    )
-      .then((response) => response.json())
-      .then((data) => {
-        setDatasetUS(data);
-      });
-    // console.log(typeof datasetUS.data[0][1], datasetUS.data, datasetUS);
-  };
   return (
     <div className="simple-bar-chart">
       bar chart here
       <svg
+        className="svg-element"
         style={{
           height: 500,
           width: "100%",
@@ -65,7 +51,6 @@ const SimpleBarChartGDP = (props) => {
         <g className="x-axis" />
         <g className="y-axis" />
       </svg>
-      <button onClick={buildGraph}>Testing button</button>
     </div>
   );
 };
