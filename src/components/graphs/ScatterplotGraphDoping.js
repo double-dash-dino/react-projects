e011ebcf03e8d54c4f2e1ff31ef0bf28288d6ce4
Lionel De Swarte--Danko (He/him) <64731565+double-dash-dino@users.noreply.github.com>
2022-01-31 17:27:28 +0000

    add scales

--- src/components/graphs/ScatterplotGraphDoping.js
+++ src/components/graphs/ScatterplotGraphDoping.js
@@ -1,59 +1,63 @@
-import React, {useState, useEffect} from 'react'
-import "./ScatterplotGraphDoping.css"
-import * as d3 from 'd3'
-
+import React, { useState, useEffect } from "react";
+import "./ScatterplotGraphDoping.css";
+import * as d3 from "d3";
 
 const ScatterplotGraphDoping = (props) => {
+  const [dataset, setDataset] = useState("");
 
+  // Get the data
+  if (dataset === "") {
+    fetch(
+      "https://raw.githubusercontent.com/freeCodeCamp/ProjectReferenceData/master/cyclist-data.json"
+    )
+      .then((response) => response.json())
+      .then((data) => {
+        setDataset(data);
+      });
+  }
 
-const [dataset, setDataset] = useState("")
+  useEffect(() => {
+    const buildChart = (dataset) => {
+      const height = 500;
+      const width = 800;
+      const padding = 20;
 
-        // Get the data
-        if (dataset === ""){
-            fetch(
-                "https://raw.githubusercontent.com/freeCodeCamp/ProjectReferenceData/master/cyclist-data.json"
-            )
-            .then((response) => response.json())
-            .then((data) => {
-                setDataset(data)
-            })
-        }
+      // Build canvas
 
+      d3.select(".scatterplot-graph")
+        .append("svg")
+        .attr("height", height)
+        .attr("width", width)
+        .attr("class", "canvas");
 
+      // Add scales
 
-useEffect(()=>{
+      const xScale = d3
+        .scaleLinear()
+        .domain([
+          d3.min(dataset, (d) => d["Year"]),
+          d3.max(dataset, (d) => d["Year"]),
+        ])
+        .range([padding, width - padding]);
+      const yScale = d3
+        .scaleLinear()
+        .domain[
+          (d3.max(dataset, (d) => d["Seconds"]),
+          d3.min(dataset, (d) => d["Seconds"]))
+        ].range([padding, height - padding]);
 
+      console.log(dataset);
+    };
 
+    if (
+      !document.getElementById("scatterplot-graph").hasChildNodes() &&
+      dataset !== ""
+    ) {
+      buildChart(dataset);
+    }
+  }, [dataset]);
 
-const buildChart = () => {
-
-    const height = 500
-    const width = 800
-    const padding = 20
-
-
-
-// Build canvas
-
-d3.select(".scatterplot-graph").append('svg').attr('height', height).attr('width', width).attr('class','canvas')
-
-
-
-console.log(dataset)
-
-}
-
-
-if (!document.getElementById("scatterplot-graph").hasChildNodes() && dataset!==""){
-buildChart(dataset)}
-
-
-},[dataset
-])
-
-
-return (<div className="scatterplot-graph" id="scatterplot-graph"></div>)
-
-}
+  return <div className="scatterplot-graph" id="scatterplot-graph"></div>;
+};
 
 export default ScatterplotGraphDoping;
