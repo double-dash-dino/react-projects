e730dc009e5d15b3242503a0a471e3b1a847b22d
Lionel De Swarte--Danko (He/him) <64731565+double-dash-dino@users.noreply.github.com>
2022-02-08 11:31:16 +0000

    set up dev envir

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
 import "./App.css";
-import React, { useState } from "react";
-import RandomQuoteApp from "./components/apps/random-quote-app.js";
-import DrumMachine from "./components/apps/drum-machine.js";
-import RandomSentenceGenerator from "./components/apps/random-sentence-generator.js";
-import NavBar from "./components/UI/NavBar";
-import ContactPage from "./components/pages/contact-page";
-import StudyTimer from "./components/apps/study-timer";
-import D3Graphs from "./components/apps/D3Graphs";
+// import React, { useState } from "react";
+// import RandomQuoteApp from "./components/apps/random-quote-app.js";
+// import DrumMachine from "./components/apps/drum-machine.js";
+// import RandomSentenceGenerator from "./components/apps/random-sentence-generator.js";
+// import NavBar from "./components/UI/NavBar";
+// import ContactPage from "./components/pages/contact-page";
+// import StudyTimer from "./components/apps/study-timer";
+// import D3Graphs from "./components/apps/D3Graphs";
+import TreemapSales from "./components/graphs/TreemapSales";
 
 function App() {
-  const [onDisplay, setOnDisplay] = useState("");
-  const selectAppHandler = (appName) => {
-    setOnDisplay(appName);
-  };
+  // const [onDisplay, setOnDisplay] = useState("");
+  // const selectAppHandler = (appName) => {
+  //   setOnDisplay(appName);
+  // };
 
-  const clearApp = () => {
-    setOnDisplay("");
-  };
+  // const clearApp = () => {
+  //   setOnDisplay("");
+  // };
   return (
     <div className="main-page">
-      <NavBar onSelectApp={selectAppHandler} />
+      <TreemapSales />
+      {/* <NavBar onSelectApp={selectAppHandler} />
       <div className="onDisplay">
         {onDisplay === "quoteApp" && <RandomQuoteApp onClearApp={clearApp} />}
         {onDisplay === "drumMachine" && <DrumMachine onClearApp={clearApp} />}
@@ -29,7 +31,7 @@ function App() {
         {onDisplay === "studyTimer" && <StudyTimer onClearApp={clearApp} />}
         {onDisplay === "d3Graphs" && <D3Graphs onClearApp={clearApp} />}
         {onDisplay === "contact" && <ContactPage />}
-      </div>
+      </div> */}
     </div>
   );
 }
