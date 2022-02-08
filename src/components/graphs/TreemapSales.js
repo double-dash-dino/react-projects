9f76f8ce5908f787b6b8f197e1307da4ca36762d
Lionel De Swarte--Danko (He/him) <64731565+double-dash-dino@users.noreply.github.com>
2022-02-08 11:21:15 +0000

    set up file structure

---
+++ src/components/graphs/TreemapSales.js
@@ -0,0 +1,9 @@
+import React, { useEffect, useState } from "react";
+import * as d3 from "d3";
+import "./TreemapSales.css";
+
+const TreemapSales = (props) => {
+  return <div className="treemap-sales" id="treemap-sales"></div>;
+};
+
+export default TreemapSales;
