a06bd078504d36546e00b0e99fc64e7a7e705410
Lionel De Swarte--Danko (He/him) <64731565+double-dash-dino@users.noreply.github.com>
2022-02-15 09:10:04 +0000

    rename env var

--- src/components/DB-projects/DBpage.js
+++ src/components/DB-projects/DBpage.js
@@ -2,7 +2,7 @@ require("dotenv").config();
 
 const DBPage = (props) => {
   const onClickButton = (event) => {
-    console.log(process.env.MONGO_DB_KEY);
+    console.log(process.env.REACT_APP_MONGO_DB_KEY);
   };
   return <button onClick={onClickButton}>Test</button>;
 };
