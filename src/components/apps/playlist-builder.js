91d0047a8fddf6452d17a12e1396b098ee8de4a7
Lionel De Swarte--Danko (He/him) <64731565+double-dash-dino@users.noreply.github.com>
2022-04-13 19:39:38 +0100

    build page wireframe

--- src/components/apps/playlist-builder.js
+++ src/components/apps/playlist-builder.js
@@ -2,7 +2,18 @@ import React, { useState } from "react";
 import "./playlist-builder.css";
 
 const PlaylistBuilder = (props) => {
-  return <div>PlaylistBuilder</div>;
+  return (
+    <div>
+      <div className="form-card">
+        Playlist builder
+        <form action="POST">
+          <input type="text" id="playlistName"></input>
+          <input type="submit" value="submit"></input>
+        </form>
+      </div>
+      <div>Playlist result</div>
+    </div>
+  );
 };
 
 export default PlaylistBuilder;
